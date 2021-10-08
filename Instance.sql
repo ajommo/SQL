4c0cd8b157095ed079148481149ca54797c087b6
ajommo <ajommorpg@gmail.com>
2021-10-08 18:25:21 +0200

    Created instance table.

---
+++ Instance.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE Instance
+(
+    Id                  UNIQUEIDENTIFIER    NOT NULL,
+    [Description]       NVARCHAR(128)       NOT NULL,
+    Asset               NVARCHAR(128)       NOT NULL,
+    Scale               BINARY(12)          NOT NULL,
+    Rotation            BINARY(12)          NOT NULL,
+    Translation         BINARY(12)          NOT NULL,
+
+    CONSTRAINT Instance_PK                  PRIMARY KEY (Id),
+    CONSTRAINT Instance_Asset_KF            FOREIGN KEY (Asset) REFERENCES Asset(Id)
+)
